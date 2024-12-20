0fae7a7f9b6f1017dc2f97cf3ae0a0f3ad19c583
moodytongytong <77366045+moodytongytong@users.noreply.github.com>
2024-12-20 00:29:36 +0000

    resets to original blog's post3 progress

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 #![no_std]
 #![no_main]
 
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-// static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    println!("Goodbye Cruel World{}", "!");
 
-    loop{}
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
